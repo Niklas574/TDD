be1b9d832b4dbb8cfc1e3777626067aec6e9e5de
Niklas574 <Niklas.Dogan@plan-b-gmbh.com>
2025-04-23 13:32:31 +0200

    FizzBuzz

---
+++ FizzBuzz/FizzBuzz/FIzzBuzzLib/FizzBuzzGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace FizzBuzzLib
+{
+    public static class FizzBuzzGame
+    {
+        public static string Play(int number)
+        {
+            return number switch
+            {
+                _ when number.IsDivisibleBy(15)
+                    => "FizzBuzz",
+                _ when number.IsDivisibleBy(3)
+                    => "Fizz",
+                _ when number.IsDivisibleBy(5)
+                    => "Buzz",
+                _ => number.ToString()
+            };
+        }
+
+        private static bool IsDivisibleBy(
+            this int i, int divisibleBy)
+            => i % divisibleBy == 0;
+    }
+}
