6903ff7937fc232bf1c384d938fc20bb26044119
Niklas574 <Niklas.Dogan@plan-b-gmbh.com>
2025-05-05 11:28:59 +0200

    FizzBuzz

---
+++ Live-Beispiel/FizzBuzz/FizzBuzzTests/FizzBuzzTest.cs
@@ -0,0 +1,15 @@
+using FizzBuzzLib;
+using FluentAssertions;
+
+namespace FizzBuzzTests
+{
+    public class FizzBuzzTest
+    {
+        // Given is an integer, return a string where:
+        // "FizzBuzz" if number is divisible by 3 and 5,
+        // "Fizz" if number is divisible by 3.
+        // "Buzz" if number is divisible by 5.
+        // number (as a string) if none of the above conditions are true
+
+    }
+}
